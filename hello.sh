cbf621037948e525fb77ca315aff9bbbb07f1372
Gabriel S. Floriano <gabrielsgf12@gmail.com>
2025-12-22 20:09:22 -0300

    update hello.sh with option modified

--- hello.sh
+++ hello.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-option=$(git branch | fzf --height 20% --layout reverse)
+option=$(git branch | fzf 
+        --height 20% 
+        --layout reverse)
+
 
 echo "Selected: option: $option"
 
-option2=$(option | tr -d "* ")
+option=$( echo $option | tr -d "* ")
 
 
-echo "Selected: option: $option2"
+echo "Select: option: $option"
 
 git switch $option
