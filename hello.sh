2d4bde5df2b1026389a07b0781da24a72d8b036d
Gabriel S. Floriano <gabrielsgf12@gmail.com>
2025-12-23 19:13:06 -0300

    update hello.sh with function logic

--- hello.sh
+++ hello.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
-#+m means the fzf command don't accept multivalue (go to more than one branch in this case)
-#-m means the accept of above explication 
-option=$(git branch | fzf +m\
-        --height 20% \
-        --layout reverse \
-        --border \
-        --preview 'git log --oneline $(echo {} | tr -d "* ")' \
-        --color bg:#222222)
+
+
+
+function avoidExitBug(){
+        #-gt is bigger than (>) 
+        #-lt is less than (<)
+        if [ $? -gt 0 ]; then
+                echo "Exiting because an error!... be carefull with commands later :)"
+                exit 1
+        fi
+}
 
 
 
 
 
-#-gt is bigger than (>) 
-#-lt is less than (<)
-if [ $? -gt 0 ]; then
-        echo "Exiting because an error!... be carefull with commands later :)"
-        exit 1
-fi
+
+function switchBranch(){
+
+        #+m means the fzf command don't accept multivalue (go to more than one branch in this case)
+        #-m means the accept of above explication 
+        option=$(git branch | fzf +m\
+                --height 20% \
+                --layout reverse \
+                --border \
+                --preview 'git log --oneline $(echo {} | tr -d "* ")' \
+                --color bg:#222222)
+
+
+
+        avoidExitBug
+
+
+
+        git switch $option
+}
 
 
 
 
-git switch $option
+
+switchBranch
