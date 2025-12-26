231b3a471589b8e1ba8cce626050315ecef43ee4
Gabriel S. Floriano <gabrielsgf12@gmail.com>
2025-12-26 14:11:20 -0300

    (develop) update hello.sh with menu function

--- hello.sh
+++ hello.sh
@@ -21,6 +21,7 @@ function switchBranch(){
         #+m means the fzf command don't accept multivalue (go to more than one branch in this case)
         #-m means accept the explication above 
         option=$(git branch | fzf +m\
+                --header "---Switching branch---" \
                 --height 100% \
                 --layout reverse \
                 --border \
@@ -41,6 +42,7 @@ function switchBranch(){
 
 function mergeBranch(){
         option=$(git branch | fzf +m \
+                --header "---Merging branch---" \
                 --height 100% \
                 --layout reverse \
                 --border \
@@ -59,9 +61,8 @@ function mergeBranch(){
 
 
 function deleteBranch(){
-        echo "you chose delete branch"
-
         option=$(git branch | fzf +m \
+                --header "---Deleting branch---" \
                 --height 100% \
                 --layout reverse \
                 --border \
@@ -73,4 +74,66 @@ function deleteBranch(){
 
 
         git branch -d "$option"
+}
+
+
+
+
+
+function main(){
+
+        menu=(\
+                "1 - switch branch" \
+                "2 - merge branch" \
+                "3 - delete branch" \
+                "Exit"\
+        )
+
+
+        selected=$(\
+                for i in "$menu[@]" ; \
+                do echo $i; \
+                done | fzf +m \
+                --height 100% \
+                --layout reverse \
+                --border \
+                --color bg:#222222
+        )
+
+
+
+        avoidExitBug
+
+
+
+
+        case "$selected" in
+                ${"$menu[0]"})
+                        switchBranch
+                        exit 0
+                ;;
+
+                ${"$menu[1]"})
+                        mergeBranch
+                        exit 0
+                ;;
+
+                ${"$menu[2]"})
+                        deleteBranch
+                        exit 0
+                ;;
+
+                ${"$menu[3]"})
+                        echo "Bye! See you later :)..."
+                        exit 0
+                ;;
+
+                *)
+                        echo "Exiting with no option!"
+                        exit 0
+
+        
+
+        esac
+
 }
