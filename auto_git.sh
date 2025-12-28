03b5e59aa2f610a75ee86fc466575721ed0e2b7c
Gabriel S. Floriano <gabrielsgf12@gmail.com>
2025-12-28 13:21:13 -0300

    insertion for avoid delete main in auto_git.sh

--- auto_git.sh
+++ auto_git.sh
@@ -83,7 +83,7 @@ function deleteBranch(){
         option=$( echo $option | tr -d "* ")
 
 
-        if [[ "$option" == "test"]]; then
+        if ["$option" = "test"]; then
                 echo "you can not delete this branch"
                 exit 0
         fi
